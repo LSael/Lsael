35519daee8a3bfa885eed51c6fd0da819e3f7aad
LSaelens <l.saelens@laposte.net>
2022-04-06 13:34:21 +0200

    Succeed to dynamicly import theme lists with Theme component

--- src/components/main/home/theme.js
+++ src/components/main/home/theme.js
@@ -9,9 +9,30 @@ const Theme = () => {
        }
     });
 
-    return ( 
-        <h1>Ceci est le theme {dataTheme[0].id}</h1>
-     );
-}
- 
+    if (dataTheme[0].articles.length > 0) {
+        return ( 
+            <div className="themeCategory">
+                <h1>Ceci est le theme {dataTheme[0].id}</h1>
+                <ul>
+                    {dataTheme[0].articles.map((article) => {
+                        return (
+                            <div className="articleButton" key={article.id}>
+                                <h3>{article.name}</h3>
+                                <p>{article.description}</p>
+                            </div>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    } else {
+        return (
+            <div className="themeCategory">
+                <h1>Ceci est le theme {dataTheme[0].id}</h1>
+                <p className="noArticle">Il n'y a pas encore d'article dans cette catégorie, veuillez nous
+                excuser pour la géne occasionnée :)</p>
+            </div>
+        )
+    }}
+
 export default Theme;
